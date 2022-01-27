d75691a469d25535d656cdda5fad8649f831c39c
Eric <EricJohnPederson@gmail.com>
2022-01-27 14:28:28 -0600

    Adding simple dependency injection.

--- src/main/java/org/example/training/Scratch.java
+++ src/main/java/org/example/training/Scratch.java
@@ -1,19 +1,19 @@
 package org.example.training;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Scratch {
 
-    public static boolean IS_AWESOME_PROGRAMMER = true;
-
     public static void main(String[] args) {
-
-        Mixer inputMixer = IS_AWESOME_PROGRAMMER ? new BackwardsMixer() : new BrokenMixer();
-        inputMixer.mix(args)
-                .forEach(System.out::println);
+        Optional.ofNullable(System.getProperties().getOrDefault("mixer-type", Mixer.Type.BROKEN.name()))
+                .map(String::valueOf)
+                .map(Mixer::getMixer)
+                .ifPresent(mixer -> mixer.mix(args).forEach(System.out::println));
     }
 
 
@@ -26,6 +26,44 @@ public class Scratch {
          * @return A list of the input in a different order.
          */
         List<String> mix(String[] input);
+
+        static Mixer getMixer(String name) {
+            try {
+                return Type.getByName(name)
+                        .getMixerClass()
+                        .getConstructor()
+                        .newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                return new BrokenMixer();
+            }
+        }
+
+        enum Type{
+            BACKWARDS(BackwardsMixer.class),
+            BROKEN(BackwardsMixer.class);
+
+            private final Class<? extends Mixer> mixerClass;
+
+            Type(Class<? extends Mixer> mixerClass) {
+                this.mixerClass = mixerClass;
+            }
+
+            public Class<? extends Mixer> getMixerClass() {
+                return mixerClass;
+            }
+
+            public static Type getByName(String name)
+            {
+                return Optional.ofNullable(name)
+                        .map(String::toUpperCase)
+                        .map(n -> Arrays.stream(values())
+                                .filter(v -> n.equals(v.name()))
+                                .findAny())
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .orElse(BROKEN);
+            }
+        }
 
     }
 
