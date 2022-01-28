b054d17b1adb58445c5cdf71da0dd8cf3a968f57
Eric Pederson <eric.pederson@biworldwide.com>
2022-01-28 16:04:36 -0600

    Creating unit tests.

--- src/main/java/org/example/training/Program.java
+++ src/main/java/org/example/training/Program.java
@@ -5,11 +5,18 @@ import org.example.training.mixer.Type;
 
 import java.util.Optional;
 
+/**
+ * Mixes command line input.
+ * Expects the class name of the {@link Mixer} implementation as a system property with a key of "mixer-type".
+ * Default implementation is the {@link org.example.training.mixer.BrokenMixer}.
+ */
 public class Program
 {
 
-    public static void main(String[] args) {
-        Optional.ofNullable(System.getProperties().getOrDefault( "mixer-type", Type.BROKEN.name()))
+    public static final String SYSTEM_PROP = "mixer-type";
+
+    public static void main( String[] args) {
+        Optional.ofNullable(System.getProperties().getOrDefault( SYSTEM_PROP, Type.BROKEN.name()))
                 .map(String::valueOf)
                 .map(Mixer::getMixer)
                 .ifPresent(mixer -> mixer.mix(args).forEach(System.out::println));
